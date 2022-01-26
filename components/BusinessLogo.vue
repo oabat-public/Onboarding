9b8b3670450182dfa001e80ff38c6faaeaf05732
Antonzo <tony.zamay@mail.ru>
2022-01-26 23:57:08 +0100

    work done

---
+++ components/BusinessLogo.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="elevation-3 business-logo">
+    <img
+      :alt="logoInfo.logoFallback"
+      :src="logoInfo.logoPath || require('@/static/images/fallback_logo.jpg')"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BusinessLogo',
+
+  props: {
+    logoInfo: {
+      type: Object,
+      default: () => ({
+        logoFallback: '',
+        logoPath: '',
+      }),
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/_variables';
+@import '@/assets/_breakpoints';
+
+.business-logo {
+  position: absolute;
+  top: -80px;
+  left: 20px;
+  background-color: $colorPrimaryWhite;
+  border-radius: 25%;
+
+  @include md-and-up {
+    top: -170px;
+    left: -20px;
+  }
+
+  img {
+    display: block;
+    overflow: hidden;
+    width: 100%;
+    height: 100%;
+    border-radius: 25%;
+  }
+}
+</style>
--- store/default.js
+++ store/default.js
@@ -2,7 +2,7 @@ export const state = () => ({
   businesses: [
     {
       id: 1,
-      logo: require('@/static/images/berg_logo.jpg'),
+      logo: '',
       preview_image: require('@/static/images/berg_preview.jpg'),
       title: 'Berg Bau',
       description:
@@ -16,13 +16,33 @@ export const state = () => ({
       is_liked: false,
     },
     {
+      id: 2,
+      logo: require('@/static/images/berg_logo.jpg'),
+      preview_image: require('@/static/images/berg_preview.jpg'),
+      title: 'Berg Bau',
+      description:
+        'Wir sind fit für die Herausforderungen von morgen und besitzen die Expertise für die Durchführung anspruchsvoller Baumaßnahmen und die Leidenschaft für die Entwicklung neuer intelligenter Lösungen, die Zukunftshorizonte eröffnen. Mit Blick auf morgen bauen und zuverlässig höchste Qualität von der Planung bis zur Fertigstellung bieten: Das war die Leitidee unseres Firmengründers Hans Berger im Jahr 1905. Aus dem Baugeschäft Hans Berger hat sich die BERGER GRUPPE entwickelt, ein familiengeführter, mittelständischer Unternehmensverbund der Bau- und Baustoffindustrie mit zahlreichen Niederlassungen, Tochter- und Beteiligungsgesellschaften im In- und Ausland und mit dem Hauptsitz in Berg am Bau. Heute sind wir eine feste Größe in der Bauindustrie.',
+      tags: [],
+      is_liked: false,
+    },
+    {
+      id: 3,
+      logo: '',
+      preview_image: '',
+      title: 'StockImages and BusinessImages Company Co. und GmbH',
+      description:
+        'Zentrale Kompetenzfelder sind anspruchsvolles Bauen, Qualitätstransportbeton-Herstellung und -Anlieferung sowie Gewinnung und Aufbereitung hochwertiger Rohstoffe. Die Bündelung spezialisierter Geschäftseinheiten ermöglicht es auch, Projekte in Komplettleistung auszuführen. Das breite Leistungsspektrum und die hohe Wertschöpfungstiefe mit eigenen Beton- und Asphaltmischwerken, einem eigenen Fertigteilwerk, modernstem Fuhr- und Gerätepark sowie eigenen Roh- und Baustoffen und nicht zuletzt 2.900 fachkompetenten BERGER Mitarbeiter garantieren höchste Qualität und beste wirtschaftliche, kundenorientierte Lösungen aus einem Guss.',
+      tags: ['Büroarbeiten', 'Anzug'],
+      is_liked: true,
+    },
+    {
       id: 4,
       logo: require('@/static/images/stock_logo.jpg'),
       preview_image: require('@/static/images/stock_preview.jpg'),
       title: 'StockImages and BusinessImages Company Co. und GmbH',
       description:
         'Zentrale Kompetenzfelder sind anspruchsvolles Bauen, Qualitätstransportbeton-Herstellung und -Anlieferung sowie Gewinnung und Aufbereitung hochwertiger Rohstoffe. Die Bündelung spezialisierter Geschäftseinheiten ermöglicht es auch, Projekte in Komplettleistung auszuführen. Das breite Leistungsspektrum und die hohe Wertschöpfungstiefe mit eigenen Beton- und Asphaltmischwerken, einem eigenen Fertigteilwerk, modernstem Fuhr- und Gerätepark sowie eigenen Roh- und Baustoffen und nicht zuletzt 2.900 fachkompetenten BERGER Mitarbeiter garantieren höchste Qualität und beste wirtschaftliche, kundenorientierte Lösungen aus einem Guss.',
-      tags: ['Büroarbeiten', 'Anzug', 'Teamarbeit', 'International'],
+      tags: ['Büroarbeiten'],
       is_liked: true,
     },
     {
@@ -35,11 +55,32 @@ export const state = () => ({
       tags: ['Flugzeuge fliegen', 'Aero', 'Teamarbeit', 'International'],
       is_liked: false,
     },
+    {
+      id: 6,
+      logo: require('@/static/images/airplane_logo.png'),
+      preview_image: require('@/static/images/airplane_preview.jpg'),
+      title: 'Airplanes Company',
+      description:
+        'Auf den unternehmerischen Grundfesten der FLUGZEUG GRUPPE, den gelebten Werten eines traditionell erfolgreichen Familienunternehmens, wächst unser Erfolg stetig und nachhaltig.',
+      tags: ['Flugzeuge fliegen', 'Aero', 'Teamarbeit', 'International'],
+      is_liked: false,
+    },
   ],
 })
 
-export const getters = {}
+export const getters = {
+  getFirstBusinesses: (state) => (number) => {
+    return state.businesses.slice(0, number)
+  },
+}
 
 export const actions = {}
 
-export const mutations = {}
+export const mutations = {
+  TOGGLE_LIKE(state, id) {
+    const targetElem = state.businesses.find((e) => {
+      return e.id === id
+    })
+    if (targetElem) targetElem.is_liked = !targetElem.is_liked
+  },
+}
